b37d15bf4a7e8a33c5d7936f9280a384df2e6d91
Vlad <ngvmtext@gmail.com>
2021-07-07 08:48:27 +0300

    init

---
+++ src/js/app.js
@@ -0,0 +1,56 @@
+var $jq = jQuery.noConflict();
+$jq(document).ready(function() {
+  // slick img
+  const sliderImgList = [
+    "url('../img/laptop.png')",
+    "url('../img/mockups.png')",
+    "url('../img/laptop.png')",
+    "url('../img/mockups.png')",
+    "url('../img/laptop.png')"
+  ];
+
+  $('.slider-wrap').on('init', function(event, slick){
+    $('#slider-img').css('background-image', sliderImgList[0]);
+  });
+  $('.slider-wrap').on('afterChange', function(event, slick, direction){
+    $('#slider-img').css('background-image', sliderImgList[direction]);
+  });
+
+  $('.slider-wrap').slick({
+    dots: true,
+    infinite: true,
+    prevArrow: '<button type="button" class="slick-prev">&#8249;</button>',
+    nextArrow: '<button type="button" class="slick-next">&#8250;</button>'
+  });
+
+  // circle spin animation active
+  $( '.top-info .images .img' ).addClass('spin');
+
+  let searchParams = new URLSearchParams(window.location.search);
+  let lng = searchParams.get('lang');
+  if(lng){
+    $('.dropdown-wrap > a').html(lng);
+  } else{
+    $('.dropdown-wrap > a').html('en');
+    lng = 'en';
+  }
+
+  $('.dropdownMenu >a').each(function( index ) {
+    const condition = $(this).text() === lng;
+    condition ? $(this).addClass('active') : $(this).removeClass('active');
+  });
+
+});
+
+// lng dropdown
+function toggle() {
+  $('.dropdown-wrap').toggleClass('opened');
+}
+function toggleMobile() {
+  $('.mobileDrop').toggleClass('opened');
+}
+
+//menu
+function toggleMenu() {
+  $('.mobile-menu').toggleClass('active');
+}
